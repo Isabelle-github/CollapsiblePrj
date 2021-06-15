f677425ea84d6107474dd42aac544128754dc6be
Isabelle-github <ixavierayix@yahoo.fr>
2021-06-15 22:32:26 +0200

    first try

---
+++ src/components/QR.js
@@ -0,0 +1,43 @@
+import Collapsible from 'react-collapsible';
+
+// let faq = [
+//     {
+//         question: "Für wen ist dieses Bootcamp?",
+//         answer:
+//             "Für alle Menschen die motiviert sind Neues zu lernen! Du solltest außerdem über 18 Jahre alt sein und bestenfalls eine Leidenschaft für den Bereich IT mitbringen.",
+//     },
+//     {
+//         question: "Welcher akademische Grad ist erforderlich?",
+//         answer:
+//             "Lange Rede, kurzer Sinn – Keiner. Wir sind auf der Suche nach wunderbaren Querdenkern*innen, die motiviert sind ein neues Leben zu starten.",
+//     },
+//     {
+//         question: "Ist Vorerfahrung im Bereich IT / Coding notwendig?",
+//         answer:
+//             "Nein. Alles was du brauchst ist Zeit, Motivation und den Willen eines Bill Gates. 😉 Du solltest aber in der Lage sein, Songs von Britney Spears zu verstehen und zu übersetzen, da alle Programmiersprachen auf Englisch basieren.",
+//     },
+//     {
+//         question: "Wie lange dauern die Bootcamps?",
+//         answer:
+//             "Das kommt ganz auf das Kursformat an. Der Vollzeit Kurs ist 22 Wochen lang (5 Monate) und der Teilzeitkurs streckt sich über 30 Wochen (7 Monate).",
+//     }
+// ]
+
+
+const QR = (props) => {
+
+    console.log(props)
+
+    return (
+        <div className='qr'>
+            <Collapsible trigger={props.data.question + `+`} triggerWhenOpen={props.data.question + "-"}>
+                <p>{props.data.answer}</p>
+                <span>&#10133;</span>
+                <span>&#10134;</span>
+            </Collapsible>
+        </div >
+
+    )
+}
+
+export default QR
